20c493f99ab4d503aaa2364f8c42ce25090d38b0
HARI PRASANTH S <hariprasanth581@gmail.com>
2024-03-06 12:52:35 +0530

    Update data.ts

--- src/lib/data/data.ts
+++ src/lib/data/data.ts
@@ -3,14 +3,14 @@ import type { Agenda1, Agenda2 } from "$lib/types/Menu";
 export const agenda1: Agenda1[] = [
     {
         title: "Hackathon kickoff + Day 1 keynote",
-        speaker: "Abirami / Panchi",
+        speaker: "Abirami Sukumaran, Google",
         start: "09:30 AM",
         end: "10:30 AM"
 
     },
     {
         title: "Phase 1 - development + Mentor assistance",
-        speaker: "-",
+        speaker: "Expert from various industries to provide guidance",
         start: "10:30 AM",
         end: "01:30 PM"
 
@@ -24,28 +24,28 @@ export const agenda1: Agenda1[] = [
     },
     {
         title: "Phase 2 - development + Mentor assistance",
-        speaker: "-",
+        speaker: "Expert from various industries to provide guidance",
         start: "02:30 PM",
         end: "04:00 PM"
 
     },
     {
         title: "Code Freeze",
-        speaker: "-",
+        speaker: "The period during which strict rules are applied for development",
         start: "04:00 PM",
         end: "05:00 PM"
 
     },
     {
         title: "Presentations",
-        speaker: "-",
+        speaker: "Showcase your poc to the panel",
         start: "05:00 PM",
         end: "07:00 PM"
 
     },
     {
         title: "Closing Note",
-        speaker: "-",
+        speaker: "Day1 Reflections ",
         start: "07:00 PM",
         end: "07:15 PM"
 
@@ -54,26 +54,26 @@ export const agenda1: Agenda1[] = [
 export const agenda2: Agenda2[] = [
     {
         title: "kickoff + keynote",
-        speaker: "Panchi",
+        speaker: "Panchi Samuthirakani, YesPanchi Tech Services Pvt Ltd",
         start: "09:30 AM",
         end: "10:00 AM"
 
     },
     {
         title: "Masterclass session - 1 (Non technical)",
-        speaker: "Vidya",
+        speaker: "Vidya Prabhu, Kyndryl",
         start: "10:00 AM",
         end: "10:40 AM"
 
     },{
         title: "Techskill - Masterclass (Technical)",
-        speaker: "Kumaresan",
+        speaker: "Kumaresan M K, Google",
         start: "10:40 AM",
         end: "11:05 AM"
 
     },{
         title: "Techskill - Masterclass (Technical)",
-        speaker: "Anisha",
+        speaker: "Anisha Udayakumar, Intel",
         start: "11:05 AM",
         end: "11:45 AM"
 
@@ -91,13 +91,13 @@ export const agenda2: Agenda2[] = [
 
     },{
         title: "Hackathon Results",
-        speaker: "-",
+        speaker: "Results of Day-1 with exciting prizes",
         start: "12:30 PM",
         end: "01:00 PM"
 
     },{
         title: "Closing Note",
-        speaker: "-",
+        speaker: "Sign Off!",
         start: "01:00 PM",
         end: "01:15 PM"
 

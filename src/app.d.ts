72642ea879a10f00ff85e8da4b9466944add05a6
nammahari <hariprasanth581@gmail.com>
2024-03-06 05:56:01 +0530

    Add Platform interface to app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -5,6 +5,15 @@ declare namespace App {
 	interface Locals {
 		userid: string;
 	}
+	interface Platform {
+		env: {
+			COUNTER: DurableObjectNamespace;
+		};
+		context: {
+			waitUntil(promise: Promise<any>): void;
+		};
+		caches: CacheStorage & { default: Cache }
+	}
 
 	// interface PageData {}
 
